d25e857d91a0b3c4482d3aba16216dd021f2691e
SBSwag10 <sbswag10@gmail.com>
2026-01-20 03:27:41 -0800

    Updates

--- _next/static/xMH_Rkj1ydvqwr4PgzUUv/_buildManifest.js
+++ _next/static/xMH_Rkj1ydvqwr4PgzUUv/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Sangeet//_next/:path+",
-        "destination": "/Sangeet/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
